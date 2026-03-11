d800bb5d3f589d1ed3a33f72408a8e8a34d7f770
carlosbuenow <carlosbueno_53@hotmail.com>
2026-03-11 17:32:53 -0300

    Subindo Atividades

---
+++ src/atividadesjava/AtividadesJAVA.java
@@ -0,0 +1,16 @@
+
+package atividadesjava;
+/*
+ @author Carlo
+ */
+import java.util.Scanner;
+
+public class AtividadesJAVA {
+
+    public static void main(String[] args) {
+       Aula04_03_26 primeiralista = new Aula04_03_26();
+       
+       primeiralista.Aula04();
+    }
+    
+}
