d300a9d6fda56cd5cac5eed45cfb960f2601ab02
carlosbuenow <carlosbueno_53@hotmail.com>
2026-04-29 22:07:12 -0300

    Arrumando design

--- Atividade_Tela/src/View/Inicio_GUI.java
+++ Atividade_Tela/src/View/Inicio_GUI.java
@@ -74,21 +74,22 @@ public class Inicio_GUI extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(0, 148, 213));
+        jPanel1.setBackground(new java.awt.Color(0, 159, 235));
         jPanel1.setForeground(new java.awt.Color(0, 50, 72));
         jPanel1.setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Tempus Sans ITC", 1, 22)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 22)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Cadastrar Cliente ");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(270, 10, 190, 30);
+        jLabel1.setBounds(0, 2, 720, 40);
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagem/usuario1.png"))); // NOI18N
         jPanel1.add(jLabel2);
         jLabel2.setBounds(38, 54, 150, 160);
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Complemento:");
         jPanel1.add(jLabel3);
@@ -96,13 +97,13 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(jTextField1);
         jTextField1.setBounds(200, 80, 260, 20);
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("RG:");
         jPanel1.add(jLabel4);
         jLabel4.setBounds(200, 110, 23, 17);
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("Data de Nascimento:");
         jPanel1.add(jLabel6);
@@ -112,7 +113,7 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(jTextField3);
         jTextField3.setBounds(200, 190, 130, 20);
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("Nacionalidade:");
         jPanel1.add(jLabel5);
@@ -120,37 +121,37 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(jTextField4);
         jTextField4.setBounds(350, 190, 110, 20);
 
-        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel8.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel8.setForeground(new java.awt.Color(255, 255, 255));
         jLabel8.setText("Nome Completo: ");
         jPanel1.add(jLabel8);
         jLabel8.setBounds(200, 60, 108, 17);
 
-        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel7.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(255, 255, 255));
         jLabel7.setText("CPF:");
         jPanel1.add(jLabel7);
         jLabel7.setBounds(480, 110, 36, 17);
 
-        jLabel9.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel9.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel9.setForeground(new java.awt.Color(255, 255, 255));
         jLabel9.setText("Sexo:");
         jPanel1.add(jLabel9);
         jLabel9.setBounds(480, 60, 36, 17);
 
-        jLabel10.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel10.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel10.setForeground(new java.awt.Color(255, 255, 255));
         jLabel10.setText("Est. Civil:");
         jPanel1.add(jLabel10);
         jLabel10.setBounds(600, 60, 108, 17);
 
-        jLabel11.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel11.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel11.setForeground(new java.awt.Color(255, 255, 255));
         jLabel11.setText("Naturalidade: ");
         jPanel1.add(jLabel11);
         jLabel11.setBounds(480, 160, 108, 17);
 
-        jLabel12.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel12.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel12.setForeground(new java.awt.Color(255, 255, 255));
         jLabel12.setText("Telefone:");
         jPanel1.add(jLabel12);
@@ -175,37 +176,37 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(jTextField6);
         jTextField6.setBounds(480, 190, 140, 20);
 
-        jLabel13.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel13.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel13.setForeground(new java.awt.Color(255, 255, 255));
         jLabel13.setText("UF:");
         jPanel1.add(jLabel13);
         jLabel13.setBounds(630, 160, 108, 17);
 
-        jLabel14.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel14.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel14.setForeground(new java.awt.Color(255, 255, 255));
         jLabel14.setText("Número:");
         jPanel1.add(jLabel14);
         jLabel14.setBounds(320, 230, 108, 17);
 
-        jLabel15.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel15.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel15.setForeground(new java.awt.Color(255, 255, 255));
         jLabel15.setText("Celular:");
         jPanel1.add(jLabel15);
         jLabel15.setBounds(170, 330, 108, 17);
 
-        jLabel16.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel16.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel16.setForeground(new java.awt.Color(255, 255, 255));
         jLabel16.setText("Município:");
         jPanel1.add(jLabel16);
         jLabel16.setBounds(40, 280, 108, 17);
 
-        jLabel17.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel17.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel17.setForeground(new java.awt.Color(255, 255, 255));
         jLabel17.setText("Bairro:");
         jPanel1.add(jLabel17);
         jLabel17.setBounds(320, 280, 108, 17);
 
-        jLabel18.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel18.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel18.setForeground(new java.awt.Color(255, 255, 255));
         jLabel18.setText("Endereço:");
         jPanel1.add(jLabel18);
@@ -223,19 +224,19 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(jTextField13);
         jTextField13.setBounds(170, 350, 120, 20);
 
-        jLabel19.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel19.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel19.setForeground(new java.awt.Color(255, 255, 255));
         jLabel19.setText("E-mail:");
         jPanel1.add(jLabel19);
         jLabel19.setBounds(320, 330, 108, 17);
 
-        jLabel21.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel21.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel21.setForeground(new java.awt.Color(255, 255, 255));
         jLabel21.setText("Cadastrado em:");
         jPanel1.add(jLabel21);
         jLabel21.setBounds(480, 330, 108, 20);
 
-        jLabel22.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel22.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
         jLabel22.setForeground(new java.awt.Color(255, 255, 255));
         jLabel22.setText("CEP:");
         jPanel1.add(jLabel22);
@@ -249,7 +250,8 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(jTextField17);
         jTextField17.setBounds(480, 350, 220, 20);
 
-        jButton1.setBackground(new java.awt.Color(0, 153, 255));
+        jButton1.setBackground(new java.awt.Color(0, 186, 226));
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setText("Limpar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -259,7 +261,8 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(jButton1);
         jButton1.setBounds(600, 380, 100, 40);
 
-        jButton2.setBackground(new java.awt.Color(255, 204, 102));
+        jButton2.setBackground(new java.awt.Color(255, 168, 77));
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
         jButton2.setText("Cadastrar");
         jPanel1.add(jButton2);
         jButton2.setBounds(480, 380, 100, 40);
@@ -285,7 +288,8 @@ public class Inicio_GUI extends javax.swing.JFrame {
             .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        pack();
+        setSize(new java.awt.Dimension(738, 468));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void MasculinoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MasculinoActionPerformed
