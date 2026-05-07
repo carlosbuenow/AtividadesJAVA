a0168b008dccf6c1476486faf87e22ca1c1bff96
carlosbuenow <carlosbueno_53@hotmail.com>
2026-05-06 22:09:05 -0300

    Utilizando Set Visible

--- Exemplo_calc/src/Model/Funcoes_DAO.java
+++ Exemplo_calc/src/Model/Funcoes_DAO.java
@@ -2,37 +2,63 @@
 package Model;
 
 import javax.swing.JOptionPane;
-import static View.Inicio_GUI.VALOR1_TXT;
-import static View.Inicio_GUI.VALOR2_TXT;
+import static View.Inicio_GUI.*;
+import java.awt.Color;
 
 public class Funcoes_DAO {
     
     public static void sair(){
         System.exit(0);
     }
-    public static int val1=0, val2=0;
+    public static int val1=0, val2=0, resp=0;
     public static void somar(){
         val1 = Integer.parseInt(View.Inicio_GUI.VALOR1_TXT.getText());
         val2 = Integer.parseInt(View.Inicio_GUI.VALOR2_TXT.getText());
-        JOptionPane.showMessageDialog(null, val1 + val2);
+        resp = val1 + val2;
+        RESULTADO_TXT.setVisible(true);
+        RESULTADO_TXT.setText(String.valueOf(resp));
+        RESULTADO_TXT.setBackground(Color.yellow);
     }
     public static void sub(){
         val1 = Integer.parseInt(View.Inicio_GUI.VALOR1_TXT.getText());
         val2 = Integer.parseInt(View.Inicio_GUI.VALOR2_TXT.getText());
-        JOptionPane.showMessageDialog(null, val1 - val2);
+        resp = val1 - val2;
+        RESULTADO_TXT.setVisible(true);
+        RESULTADO_TXT.setText(String.valueOf(resp));
+        RESULTADO_TXT.setBackground(Color.yellow);
     }
     public static void div(){
         val1 = Integer.parseInt(View.Inicio_GUI.VALOR1_TXT.getText());
         val2 = Integer.parseInt(View.Inicio_GUI.VALOR2_TXT.getText());
-        JOptionPane.showMessageDialog(null, val1 / val2);
+        resp = val1 / val2;
+        RESULTADO_TXT.setVisible(true);
+        RESULTADO_TXT.setText(String.valueOf(resp));
+        RESULTADO_TXT.setBackground(Color.yellow);
     }
     public static void mult(){
         val1 = Integer.parseInt(View.Inicio_GUI.VALOR1_TXT.getText());
         val2 = Integer.parseInt(View.Inicio_GUI.VALOR2_TXT.getText());
-        JOptionPane.showMessageDialog(null, val1 * val2);
+        resp = val1 * val2;
+        RESULTADO_TXT.setVisible(true);
+        RESULTADO_TXT.setText(String.valueOf(resp));
+        RESULTADO_TXT.setBackground(Color.yellow);
+    }
+    public static void porc(){
+        val1 = Integer.parseInt(View.Inicio_GUI.VALOR1_TXT.getText());
+        val2 = Integer.parseInt(View.Inicio_GUI.VALOR2_TXT.getText());
+        resp = (val1 * val2) / 100;
+        RESULTADO_TXT.setVisible(true);
+        RESULTADO_TXT.setText(String.valueOf(resp));
+        RESULTADO_TXT.setBackground(Color.yellow);
+    
+    }
+    public static void potencia(){
+        
     }
     public static void limpar(){
+        RESULTADO_TXT.setVisible(false);
         VALOR1_TXT.setText("");
         VALOR2_TXT.setText("");
+        RESULTADO_TXT.setText("");
     }
 }
--- Exemplo_calc/src/View/Inicio_GUI.java
+++ Exemplo_calc/src/View/Inicio_GUI.java
@@ -21,13 +21,17 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         VALOR1_TXT = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        VALOR2_TXT = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        RESULTADO_TXT = new javax.swing.JTextField();
         sair_btn = new javax.swing.JButton();
         soma_btn = new javax.swing.JButton();
         sub_btn = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        mult_btn = new javax.swing.JButton();
         limpar_btn = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        VALOR2_TXT = new javax.swing.JTextField();
+        porc_btn = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        potencia_btn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -38,39 +42,28 @@ public class Inicio_GUI extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Valor 1");
+        jLabel1.setText("Resultado");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(220, 60, 90, 40);
+        jLabel1.setBounds(210, 210, 90, 40);
 
         VALOR1_TXT.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jPanel1.add(VALOR1_TXT);
-        VALOR1_TXT.setBounds(210, 120, 80, 28);
+        VALOR1_TXT.setBounds(210, 70, 80, 28);
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Valor 2");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(220, 170, 80, 30);
+        jLabel2.setBounds(210, 120, 80, 30);
 
-        VALOR2_TXT.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        VALOR2_TXT.addActionListener(new java.awt.event.ActionListener() {
+        RESULTADO_TXT.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        RESULTADO_TXT.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                VALOR2_TXTActionPerformed(evt);
+                RESULTADO_TXTActionPerformed(evt);
             }
         });
-        jPanel1.add(VALOR2_TXT);
-        VALOR2_TXT.setBounds(210, 220, 80, 28);
-
-        jButton1.setBackground(new java.awt.Color(255, 255, 255));
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton1.setText("/");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton1);
-        jButton1.setBounds(410, 350, 60, 60);
+        jPanel1.add(RESULTADO_TXT);
+        RESULTADO_TXT.setBounds(210, 260, 80, 28);
 
         sair_btn.setBackground(new java.awt.Color(255, 255, 255));
         sair_btn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
@@ -86,35 +79,41 @@ public class Inicio_GUI extends javax.swing.JFrame {
         soma_btn.setBackground(new java.awt.Color(255, 255, 255));
         soma_btn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         soma_btn.setText("+");
+        soma_btn.setMaximumSize(new java.awt.Dimension(43, 31));
+        soma_btn.setMinimumSize(new java.awt.Dimension(43, 31));
+        soma_btn.setPreferredSize(new java.awt.Dimension(43, 31));
         soma_btn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 soma_btnActionPerformed(evt);
             }
         });
         jPanel1.add(soma_btn);
-        soma_btn.setBounds(50, 350, 60, 60);
+        soma_btn.setBounds(20, 360, 60, 50);
 
         sub_btn.setBackground(new java.awt.Color(255, 255, 255));
         sub_btn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         sub_btn.setText("-");
+        sub_btn.setMaximumSize(new java.awt.Dimension(43, 31));
+        sub_btn.setMinimumSize(new java.awt.Dimension(43, 31));
+        sub_btn.setPreferredSize(new java.awt.Dimension(43, 31));
         sub_btn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 sub_btnActionPerformed(evt);
             }
         });
         jPanel1.add(sub_btn);
-        sub_btn.setBounds(170, 350, 60, 60);
+        sub_btn.setBounds(100, 360, 60, 50);
 
-        jButton5.setBackground(new java.awt.Color(255, 255, 255));
-        jButton5.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton5.setText("*");
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
+        mult_btn.setBackground(new java.awt.Color(255, 255, 255));
+        mult_btn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        mult_btn.setText("*");
+        mult_btn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
+                mult_btnActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton5);
-        jButton5.setBounds(290, 350, 60, 60);
+        jPanel1.add(mult_btn);
+        mult_btn.setBounds(180, 360, 60, 50);
 
         limpar_btn.setBackground(new java.awt.Color(255, 255, 255));
         limpar_btn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
@@ -127,6 +126,59 @@ public class Inicio_GUI extends javax.swing.JFrame {
         jPanel1.add(limpar_btn);
         limpar_btn.setBounds(400, 60, 100, 30);
 
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Valor 1");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(210, 30, 90, 40);
+
+        VALOR2_TXT.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        VALOR2_TXT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                VALOR2_TXTActionPerformed(evt);
+            }
+        });
+        jPanel1.add(VALOR2_TXT);
+        VALOR2_TXT.setBounds(210, 160, 80, 28);
+
+        porc_btn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        porc_btn.setText("%");
+        porc_btn.setPreferredSize(new java.awt.Dimension(43, 31));
+        porc_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                porc_btnActionPerformed(evt);
+            }
+        });
+        jPanel1.add(porc_btn);
+        porc_btn.setBounds(340, 360, 60, 50);
+
+        jButton3.setBackground(new java.awt.Color(255, 255, 255));
+        jButton3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jButton3.setText("/");
+        jButton3.setMaximumSize(new java.awt.Dimension(43, 31));
+        jButton3.setMinimumSize(new java.awt.Dimension(43, 31));
+        jButton3.setPreferredSize(new java.awt.Dimension(43, 31));
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton3);
+        jButton3.setBounds(260, 360, 60, 50);
+
+        potencia_btn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        potencia_btn.setText("^");
+        potencia_btn.setMaximumSize(new java.awt.Dimension(43, 31));
+        potencia_btn.setMinimumSize(new java.awt.Dimension(43, 31));
+        potencia_btn.setPreferredSize(new java.awt.Dimension(43, 31));
+        potencia_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                potencia_btnActionPerformed(evt);
+            }
+        });
+        jPanel1.add(potencia_btn);
+        potencia_btn.setBounds(420, 360, 60, 50);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -135,16 +187,16 @@ public class Inicio_GUI extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 451, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 451, Short.MAX_VALUE)
         );
 
         setSize(new java.awt.Dimension(519, 451));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void VALOR2_TXTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_VALOR2_TXTActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_VALOR2_TXTActionPerformed
+    private void RESULTADO_TXTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RESULTADO_TXTActionPerformed
+
+    }//GEN-LAST:event_RESULTADO_TXTActionPerformed
 
     private void sair_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sair_btnActionPerformed
         Model.Funcoes_DAO.sair();
@@ -158,17 +210,29 @@ public class Inicio_GUI extends javax.swing.JFrame {
         Model.Funcoes_DAO.sub();
     }//GEN-LAST:event_sub_btnActionPerformed
 
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+    private void mult_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mult_btnActionPerformed
         Model.Funcoes_DAO.mult();
-    }//GEN-LAST:event_jButton5ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Model.Funcoes_DAO.div();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_mult_btnActionPerformed
 
     private void limpar_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limpar_btnActionPerformed
         Model.Funcoes_DAO.limpar();
     }//GEN-LAST:event_limpar_btnActionPerformed
+
+    private void VALOR2_TXTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_VALOR2_TXTActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_VALOR2_TXTActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        Model.Funcoes_DAO.div();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void porc_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_porc_btnActionPerformed
+        Model.Funcoes_DAO.porc();
+    }//GEN-LAST:event_porc_btnActionPerformed
+
+    private void potencia_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_potencia_btnActionPerformed
+       Model.Funcoes_DAO.potencia();
+    }//GEN-LAST:event_potencia_btnActionPerformed
 
     public static void main(String args[]) {
         try {
@@ -196,14 +260,18 @@ public class Inicio_GUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.JTextField RESULTADO_TXT;
     public static javax.swing.JTextField VALOR1_TXT;
     public static javax.swing.JTextField VALOR2_TXT;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton limpar_btn;
+    private javax.swing.JButton mult_btn;
+    private javax.swing.JButton porc_btn;
+    private javax.swing.JButton potencia_btn;
     private javax.swing.JButton sair_btn;
     private javax.swing.JButton soma_btn;
     private javax.swing.JButton sub_btn;
